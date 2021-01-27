bae09ef538a4503898ee44329818d743a271c428
DamonLiu <damon.liu@cyan.com.tw>
2021-01-27 14:29:26 +0800

    [Test] 以 PointType&DataType 改寫 read 測說

--- test_tool/read_modbus.py
+++ test_tool/read_modbus.py
@@ -3,32 +3,19 @@ import click
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 from pymodbus.exceptions import ConnectionException
 
+from modbus_types import PointType, DataType
 
-def read(*point_addr_len_list, ip=None, port=None):
+
+def read(*point_addr_data_list, ip=None, port=None):
     client = ModbusClient(ip, port=port)
     client.connect()
+    if not client.socket: return []*len(point_addr_data_list)
     
     val_list = []
-    for pt,addr,l in point_addr_len_list:
-        if l > 1:
-            l /= 2
-            l = int(l)
-    
-        if pt == 'co':
-            rr = client.read_coils(addr, l)
-            val = rr.bits if not rr.isError() else 'err'
-        elif pt == 'di':
-            rr = client.read_discrete_inputs(addr, l)
-            val = rr.bits if not rr.isError() else 'err'
-        elif pt == 'hr':
-            rr = client.read_holding_registers(addr, l)
-            val = rr.registers if not rr.isError() else 'err'
-        elif pt == 'ir':
-            rr = client.read_input_registers(addr, l)
-            val = rr.registers if not rr.isError() else 'err'
-        
-        if l == 1: val = val[0]
-        val_list.append(val)
+    for pt,addr,data in point_addr_data_list:
+        req,vals = pt.RequestValue(client, addr, count=data.length)
+        if req:
+            val_list.append(data.Decode(vals))
 
     client.close()
     return val_list
@@ -39,28 +26,33 @@ def read(*point_addr_len_list, ip=None, port=None):
 @click.option('--ip', type=str)
 @click.option('--port', type=int, default=502)
 def read_loop(point, intval, ip, port):
-    pts = point[::2]
-    addr_len_list = [addr_len for addr_len in point[1::2]]
+    pts = [PointType(pt) for pt in point[::2]]
+    addr_data_list = point[1::2]
     addrs = []
-    len_list = []
-    for al in addr_len_list:
+    data_list = []
+    for i,al in enumerate(addr_data_list):
         if ':' in al:
             a,l = al.split(':')
             addrs.append(int(a))
-            len_list.append(int(l))
+            data_list.append(DataType(l, pts[i]))
         else:
             addrs.append(int(al))
-            len_list.append(1)
+            data_list.append(DataType('bool', pts[i]))
 
     while True:
         try:
-            val_list = read(*zip(pts, addrs, len_list), ip=ip, port=port)
-            info_str = ip + ''.join(f'[{pt}_{addr}] {val}'.ljust(15) for pt,addr,val in zip(pts, addrs, val_list))
+            val_list = read(*zip(pts, addrs, data_list), ip=ip, port=port)
+            info_str = ip + ' '.join(f'[{pt}_{addr}:{data.type_str}]{val}'.ljust(15) for pt,addr,data,val in zip(pts, addrs, data_list, val_list))
             print(info_str)
             time.sleep(intval)
         except ConnectionException:
-            print('connection loss...')
-            time.sleep(30)
+            print('connection loss...', end='')
+            time.sleep(50)
+            print('retry')
+        
+        except Exception as e:
+            print(e)
+            break
 
 if __name__ == "__main__":
     read_loop()
