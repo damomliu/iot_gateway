9834ebf2814fd518ec7ff1186b08142090e7e4a0
DamonLiu <damon.liu@cyan.com.tw>
2021-02-01 11:57:45 +0800

    [Test] write_modbus

--- test_tool/write_modbus.py
+++ test_tool/write_modbus.py
@@ -2,33 +2,38 @@ import time
 import random
 import click
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+from modbus_types import PointType, DataType
 
 
 def write(*point_addr_value_list, ip='127.0.0.1', port=502):
     client = ModbusClient(ip, port=port)
     client.connect()
     
-    for pt,addr,val in point_addr_value_list:
-        if pt == 'co':
-            client.write_coil(addr, bool(val))
-        elif pt == 'hr':
-            client.write_register(addr, val)
+    for pt,addr,val,dt in point_addr_value_list:
+        pt = PointType(pt)
+        if dt.startswith('int'):
+            val = int(val)
+        elif dt.startswith('float'):
+            val = float(val)
+        elif dt.startswith('bool'):
+            val = int(val) != 0
+        
+        dt = DataType(dt)
+        writeFunc = pt._WriteFunc(client)
+        req = writeFunc(addr, dt.Encode(val))
+        print(not req.isError())
 
     client.close()
 
 @click.command()
 @click.argument('point')
 @click.argument('addr', type=int)
-@click.argument('btm', type=int, default=0)
-@click.argument('top', type=int, default=1)
-@click.argument('intval', type=float, default=1)
+@click.argument('val')
+@click.argument('datatype', type=str, default='float32')
 @click.option('--ip', type=str, default='127.0.0.1')
 @click.option('--port', type=int, default=502)
-def random_loop(point, addr, btm, top, intval, ip, port):
-    # while True:
-        newval = random.randint(btm, top)
-        write((point, addr, newval), ip=ip, port=port)
-        time.sleep(intval)
+def random_loop(point, addr, val, datatype, ip, port):
+    write((point, addr, val, datatype), ip=ip, port=port)
 
 if __name__ == "__main__":
     random_loop()
