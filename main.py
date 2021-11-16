d16edd768030a03d914952db763143d6b0922175
DamonLiu_iMac <damon.liu@cyan.com.tw>
2021-11-16 12:16:29 +0800

    fix: :bug: 修正 logger 無法正確代入 CTRL

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     LOGGER = OneLogger('CTRL', log_path=LOG_PATH, rotate=True)
     LOGGER.info(f'===== Starting ModbusController {ModbusController.__version__} =====')
 
-    CTRL = ModbusController.FromConfigFile(CONFIG_PATH)
+    CTRL = ModbusController.FromConfigFile(CONFIG_PATH, logger=LOGGER)
     CTRL.verbose = True
     CTRL.Start()
