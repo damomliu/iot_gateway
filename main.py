4f7bce83a7eece55459604f77cde461fc3515ebd
JimmyHub <j29241919@gmail.com>
2022-04-19 17:53:57 +0800

    refactor: :recycle: model.Config 新增巢狀結構

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from logger import OneLogger
 
 
 LOG_PATH = '../log/ctrl.log'
-# CONFIG_PATH = '../config/ctrl_config.json'
-CONFIG_PATH = 'test.db'
+CONFIG_PATH = '../config/ctrl_config.json'
+# CONFIG_PATH = 'test.db'
+
 if __name__ == "__main__":
     LOGGER = OneLogger('CTRL', log_path=LOG_PATH, rotate=True)
     LOGGER.info(f'===== Starting ModbusController {ModbusController.__version__} =====')
--- model/config.py
+++ model/config.py
@@ -1,32 +1,34 @@
 import os
 import sqlite3
-from pydantic import BaseModel
-
-
-class SourceConfig(BaseModel):
-    address_path: str = './address.csv'
-    register_folder: str = './.register/'
-    addr_start_from: int = 1
-    source_port: int = 502
-    source_sid: int = 0x01
-    pointtype_str: str = 'hr'
-    datatype_str: str = 'float32'
-    abcd_str: str = 'ABCD'
-    server_host: str = '127.0.0.1'
-    server_port: int = 5020
-    server_sid: int = 0x00
-    server_null_value: int = -99
-    mirror_refresh_sec: float = 0.5
-    mirror_retry_sec: int = 10 * 60
-    readwrite_retry_sec: int = 10 * 60
-    shutdown_delay_sec: int = 0
+from pydantic import BaseModel, Field
 
 
 class OpcuaConfig(BaseModel):
-    separator: str
-    security: bool
-    crt_path: str
-    key_path: str
+    separator: str = Field(alias='separator', default='+-')
+    security: bool = Field(alias='security', default=True)
+    crt_path: str = Field(alias='crt_path', default='../config/security/client_crt.der')
+    key_path: str = Field(alias='key_path', default='../config/security/client_key.pem')
+
+
+class SourceConfig(BaseModel):
+    address_path: str = Field(alias='address_path', default='./address.csv')
+    register_folder: str = Field(alias='address_path', default='./.register/')
+    addr_start_from: int = Field(alias='addr_start_from', default=1)
+    source_port: int = Field(alias='source_port', default=502)
+    source_sid: int = Field(alias='source_sid', default=0x01)
+    pointtype_str: str = Field(alias='pointtype_str', default='hr')
+    datatype_str: str = Field(alias='datatype_str', default='float32')
+    abcd_str: str = Field(alias='abcd_str', default='ABCD')
+    server_host: str = Field(alias='server_host', default='127.0.0.1')
+    server_port: int = Field(alias='server_port', default=5020)
+    server_sid: int = Field(alias='server_sid', default=0x00)
+    server_null_value: int = Field(alias='server_null_value', default=-99)
+    mirror_refresh_sec: float = Field(alias='mirror_refresh_sec', default=0.5)
+    mirror_retry_sec: int = Field(alias='mirror_retry_sec', default=10 * 60)
+    readwrite_retry_sec: int = Field(alias='readwrite_retry_sec', default=10 * 60)
+    shutdown_delay_sec: int = Field(alias='shutdown_delay_sec', default=0)
+    opcua: OpcuaConfig = Field(alias='opcua', default=None)
+
 
 class MetaSingleTon(type):
     _instances = {}
@@ -35,6 +37,7 @@ class MetaSingleTon(type):
         if cls not in cls._instances:
             cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
+
 
 class Config(SourceConfig):
     @classmethod
@@ -45,10 +48,17 @@ class Config(SourceConfig):
         con.row_factory = sqlite3.Row
         cur = con.cursor()
         cur.execute('select * from config')
-        config_dict = cur.fetchone()
+        config_dict = dict(cur.fetchone())
+        cls.form_nest_sqlite(cur, config_dict, 'opcua', 'opcua')
         cur.close()
         con.close()
         return cls(**config_dict)
+    @classmethod
+    def form_nest_sqlite(self,cur,st_dict,nd_table,nd_key):
+        cur.execute(f'select * from {nd_table} where id ={st_dict[nd_key]}')
+        opcua_dict = cur.fetchone()
+        st_dict[nd_key] = opcua_dict
+
     @classmethod
     def create(cls, config_path):
         ext = os.path.splitext(config_path)[-1]
@@ -62,7 +72,5 @@ class Config(SourceConfig):
 
 
 if __name__ == '__main__':
-
     res_sql = Config.from_sqllite('../test.db').dict()
     print('sql:', res_sql)
-
